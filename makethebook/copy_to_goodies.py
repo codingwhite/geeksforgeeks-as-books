7a68682785e5a3f7cb3657e2a750907dc845526e
gnijuohz <gnijuohz@gmail.com>
2015-02-22 12:27:14 -0700

    new verision of the books

--- makethebook/copy_to_goodies.py
+++ makethebook/copy_to_goodies.py
@@ -1,6 +1,6 @@
 import glob
 from subprocess import call
 for book_format in ['mobi', 'epub']:
-    for book in glob.glob("./*/*."+book_format):
+    for book in glob.glob("./*/*1.1."+book_format):
         print "coping " + book + " to ../goodies/"
         call("cp " + book + " ../goodies/", shell=True)
--- makethebook/generate_book.py
+++ makethebook/generate_book.py
@@ -9,26 +9,27 @@ import glob
 from clean import clean_html_files
 
 
-def generate(book):
-    print "cleaning html files", "...might take a while.."
+def generate(book, version):
+    print "cleaning html files,", "this might take a while..."
     clean_html_files(book)
     cleaned_html = sorted(glob.glob(book + "*_cleaned.html"))
-    md_file = book + book[:-1] + ".md"
-    html_file = book + book[:-1] + ".html"
-    epub_file = book + book[:-1] + ".epub"
-
+    md_file = book + book[:-1] + version + ".md"
+    html_file = book + book[:-1] + version + ".html"
+    epub_file = book + book[:-1] + version + ".epub"
     print "generating", html_file, "..."
-    call("cat " +  " ".join(cleaned_html) + " > " + html_file, shell=True)
-    print "generating", epub_file, "...might take a while..."
+    call("cat " + "../bookcopyright/copyright.html " + " ".join(cleaned_html) + " > " + html_file, shell=True)
+    print "generating", epub_file, "this might take a while..."
     call(['pandoc', '-o', epub_file, html_file, '--epub-metadata='+book+'metadata.xml', "--toc", "--toc-depth=2", "--epub-stylesheet=../styles/buttondown.css", "-f", "html-native_divs"])
-    print "generating", book + book[:-1] + ".mobi", "..."
+    print "generating", book + version + book[:-1] + ".mobi", "..."
     call(['kindlegen', epub_file])
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print "this program takes one argument"
         sys.exit()
     book = sys.argv[1]
+    version = sys.argv[2]
     if book[-1] != "/":
         book = book + "/"
-    generate(book)
+    version = "_" + version
+    generate(book, version)
